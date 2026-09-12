543efc923646da386639195ba1ef7b1a0cc7bdc5
Denise Ribeiro <denise.ribeiro@mail.usf.edu.br>
2026-09-12 18:05:49 -0300

    Enforce benchmark-specific requirements

--- scripts/run_benchmark.R
+++ scripts/run_benchmark.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+source(file.path("scripts", "check_benchmark_requirements.R"), local = new.env(parent = globalenv()))
 source(file.path("scripts", "benchmark_run.R"))
 source(file.path("scripts", "benchmark_analyze.R"))
 source(file.path("scripts", "07_export_environment.R"))
