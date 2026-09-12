34ee65fea1b8fe4e77d47279764e4458184a90d3
Denise Ribeiro <denise.ribeiro@mail.usf.edu.br>
2026-09-12 18:07:10 -0300

    Use benchmark-only environment capture

--- scripts/run_benchmark.R
+++ scripts/run_benchmark.R
@@ -3,4 +3,4 @@
 source(file.path("scripts", "check_benchmark_requirements.R"), local = new.env(parent = globalenv()))
 source(file.path("scripts", "benchmark_run.R"))
 source(file.path("scripts", "benchmark_analyze.R"))
-source(file.path("scripts", "07_export_environment.R"))
+source(file.path("scripts", "benchmark_export_environment.R"))
